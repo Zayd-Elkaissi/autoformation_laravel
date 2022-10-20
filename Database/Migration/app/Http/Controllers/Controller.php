747f4f171e9ccc54e75cd7d86b79c962b3752729
Zayd-Elkaissi <zayd.elkaissi.solicode@gmail.com>
2022-10-20 15:54:36 +0100

    database

--- Database/Migration/app/Http/Controllers/Controller.php
+++ Database/Migration/app/Http/Controllers/Controller.php
@@ -8,13 +8,19 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
+use Illuminate\Support\Facades\DB;
 
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
 
 
-
+    public function index()
+    {
+        $users = DB::table('users')->get();
+ 
+        return view('user.index', ['users' => $users]);
+    }
 }
 
 
